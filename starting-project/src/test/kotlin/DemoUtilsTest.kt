16ad1ddc273d8652bd19b02c60f2c608b07da4cc
mohamedtamer0 <mt4452242@gmail.com>
2024-03-01 11:43:06 +0200

    junit

--- starting-project/src/test/kotlin/DemoUtilsTest.kt
+++ starting-project/src/test/kotlin/DemoUtilsTest.kt
@@ -1,21 +1,49 @@
 import org.example.DemoUtils
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 class DemoUtilsTest {
+
+    var demoUtils = DemoUtils()
+
+    @BeforeEach
+    fun setupBeforeEach() {
+        demoUtils = DemoUtils()
+        println("@BeforeEach executes before the execution of each test method")
+    }
+
+    @AfterEach
+    fun setupAfterEach() {
+        demoUtils = DemoUtils()
+        println("@AfterEach")
+        println()
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun setupBeforeAll() {
+            println("@BeforeAll")
+            println()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun setupAfterAll() {
+            println("@AfterAll")
+        }
+    }
+
+
     @Test
     fun testEqualsAndNotEquals() {
-        val demoUtils = DemoUtils()
         Assertions.assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6")
         Assertions.assertNotEquals(6, demoUtils.add(1, 4), "2+4 must not be 6")
     }
 
     @Test
     fun testNullAndNotNull() {
-        val demoUtils = DemoUtils()
         val str1: String? = null
         val str2 = "Tamer"
-
         Assertions.assertNull(demoUtils.checkNull(str1), "Object should be null")
         Assertions.assertNotNull(demoUtils.checkNull(str2), "Object should not be null")
     }
